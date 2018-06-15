f83f23940a49e4967d0e6b015c4eb71207cbda9e
sk <semaria.eyzac@gmail.com>
2018-06-15 13:11:13 -0700

    add and fail tests to check the deleteById and the clearAll methods works perfectly

--- Hikes/src/test/java/dao/Sql2oHikeDaoTest.java
+++ Hikes/src/test/java/dao/Sql2oHikeDaoTest.java
@@ -92,7 +92,7 @@ public class Sql2oHikeDaoTest {
         Hike hike = setupNewHike();
         hikeDao.add(hike);
         hikeDao.deleteById(hike.getId());
-        assertEquals(0, hikeDao.getAll().size());
+        assertEquals(2, hikeDao.getAll().size());
     }
 
     @Test
@@ -103,7 +103,7 @@ public class Sql2oHikeDaoTest {
         hikeDao.add(otherHike);
         int daoSize = hikeDao.getAll().size();
         hikeDao.clearAllHikes();
-        assertTrue(daoSize>0 && daoSize > hikeDao.getAll().size());
+        assertFalse(daoSize>0 && daoSize > hikeDao.getAll().size());
     }
 
     private Hike setupNewHike() {
