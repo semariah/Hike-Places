e44b55cd6bbb1de55c7703448d65a37364e06b6f
sk <semaria.eyzac@gmail.com>
2018-06-15 11:04:47 -0700

    create Sql2o for userdao and hike dao files and add some tests in the Sql2oHikeDaoTest file for the methods in the HikeDao interface file

--- Hikes/src/test/java/dao/Sql2oHikeDaoTest.java
+++ Hikes/src/test/java/dao/Sql2oHikeDaoTest.java
@@ -1,9 +1,12 @@
 package dao;
 
+import models.Hike;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import static org.junit.Assert.*;
 
 public class Sql2oHikeDaoTest {
     private Sql2oHikeDao hikeDao;
@@ -21,5 +24,32 @@ public class Sql2oHikeDaoTest {
     public void tearDown() throws Exception {
         conn.close();
 
+    }
+
+    @Test
+    public void addingHikesSetsId() throws Exception{
+        Hike hike = setupNewHike();
+        int originalHikeId = hike.getId();
+        hikeDao.add(hike);
+        assertNotEquals(originalHikeId, hike.getId());
+    }
+
+    @Test
+    public void existingHikesFoundById() throws Exception {
+        Hike hike = setupNewHike();
+        hikeDao.add(hike);
+        Hike foundHike = hikeDao.findById(hike.getId());
+        assertEquals(foundHike, hike);
+    }
+
+    @Test
+    public void getAllGetsAllAddedHikes() {
+        Hike hike = setupNewHike();
+        hikeDao.add(hike);
+        assertEquals(1, hikeDao.getAll().size());
+    }
+
+    private Hike setupNewHike() {
+        return new Hike("semhar", 2, "oregon");
     }
 }
