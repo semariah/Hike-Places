0f572e5d51b3dfbbd464c4a0ec98e1185c0a17dc
sk <semaria.eyzac@gmail.com>
2018-06-15 11:10:14 -0700

    add my sql2o object to be available inorder to be able to call methods and initialize it as private final implements field and add override methods for getAll, add and findById methods

--- Hikes/src/main/java/dao/Sql2oHikeDao.java
+++ Hikes/src/main/java/dao/Sql2oHikeDao.java
@@ -1,4 +1,29 @@
 package dao;
 
-public class Sql2oHikeDao {
+import models.Hike;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oHikeDao implements HikeDao{
+    private final Sql2o sql2o;
+
+    public Sql2oHikeDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public List<Hike> getAll() {
+        return null;
+    }
+
+    @Override
+    public void add(Hike hike) {
+
+    }
+
+    @Override
+    public Hike findById(int id) {
+        return null;
+    }
 }
