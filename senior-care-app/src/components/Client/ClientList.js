d56fec52428c1e99a7491d78147b771e7acd6e8e
sk <semaria.eyzac@gmail.com>
2018-07-13 14:25:28 -0700

    fix a bug preventing loading

--- senior-care-app/src/components/Client/ClientList.js
+++ senior-care-app/src/components/Client/ClientList.js
@@ -3,16 +3,16 @@ import Client from './Client'
 
 var masterClientList = [
   {
-    name: "thomaas",
-    address: "NE Beech portland",
-    tel: "971-234-5678",
-    service: "home-cleaning"
+    name: 'thomaas',
+    address: 'NE Beech portland',
+    tel: '971-234-5678',
+    service: 'home-cleaning'
   },
   {
-    name: "saari",
-    address: "california",
-    tel: "971-234-9807",
-    service: "bathing"
+    name: 'saari',
+    address: 'california',
+    tel: '971-234-9807',
+    service: 'bathing'
   },
 
 ]
