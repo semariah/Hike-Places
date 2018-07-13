6e38311313dba983883605b090a5df8d07345742
sk <semaria.eyzac@gmail.com>
2018-07-13 13:18:06 -0700

    move the App.css syle page inside the component folder

--- senior-care-app/src/components/App.js
+++ senior-care-app/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import App from './components/App.css'
+import './App.css'
 import ClientList from './Client/ClientList'
 
 
