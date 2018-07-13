8c3886ce3d899d5a5d26f07eadbbedb82839f628
sk <semaria.eyzac@gmail.com>
2018-07-13 16:47:07 -0700

    add the event handler in the clientform file to create form for a new client

--- senior-care-app/src/components/client/ClientForm.js
+++ senior-care-app/src/components/client/ClientForm.js
@@ -1,23 +1,45 @@
 import React from 'react';
 
 function ClientForm(){
+  let _name = null;
+  let _address = null;
+  let _tel = null;
+  let _service = null;
+
+  function handleClientFormSubmission(event) {
+    event.preventDefault();
+    console.log(_name.value);
+    console.log(_address.value);
+    console.log(_tel.value);
+    console.log(_service.value);
+    _name.value = '';
+    _address.value = '';
+    _tel.value = '';
+    _service.value = '';
+
+
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleClientFromSubmission}>
         <input
           type='text'
           id='name'
-          placeholder= 'Name'/>
+          placeholder= 'Name'
+          ref={(input) => {_name = input;}}/>
         <input
           type='text'
           id='address'
-          placeholder='Address'/>
+          placeholder='Address'
+          ref={(input) => {_address = input;}}/>
         <input
           id='tel'
-          placeholder='Phone'/>
+          placeholder='Phone'
+          ref={(input) => {_tel = input;}}/>
           <input
             id='service'
-            placeholder='Service Needed'/>
+            placeholder='Service Needed'
+            ref={(input) => {_service = input;}}/>
         <button type='submit'>Submit</button>
       </form>
     </div>
